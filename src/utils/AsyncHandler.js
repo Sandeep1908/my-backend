da1e28e12d348afee927e0aa8b589bc83b865c5b
sandeep <sandbro7163337@gmail.com>
2023-11-28 17:52:37 +0530

    create utilites

---
+++ src/utils/AsyncHandler.js
@@ -0,0 +1,19 @@
+
+
+const asyncHandler= (requestHandler) => {
+    return (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+}
+
+
+
+const asyncHandler2=(requestHandler)=>async(req,res,next)=>{
+    try {
+        return await requestHandler(req,res,next)
+    } catch (err) {
+        res.status(err.code || 500).json({success:false,message:err.message})
+    }
+}
+
+export {asyncHandler}
